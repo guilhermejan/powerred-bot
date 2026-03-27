fa1143ad5e5d17296475444e722cc8cea3c3afe2
Guilherme <guilhermepower70@gmail.com>
2026-03-27 13:32:18 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+const TOKEN = process.env.TOKEN;
+
+const SOURCE_CHANNEL_ID = '1354297191094554776';
+const TARGET_CHANNEL_ID = '1483822397374206155';
+
+client.on('clientReady', () => {
+  console.log(`✅ Bot online como ${client.user.tag}`);
+});
+
+client.on('messageCreate', async (message) => {
+  if (message.channel.id !== SOURCE_CHANNEL_ID) return;
+  if (message.author.bot) return;
+
+  try {
+    const targetChannel = await client.channels.fetch(TARGET_CHANNEL_ID);
+    if (!targetChannel) return;
+
+    await targetChannel.send({
+  content: `@everyone\n📩 ${message.content || `Mensagem de ${message.author.tag}`}`,
+  embeds: message.embeds,
+  files: message.attachments.map(att => att.url)
+    });
+  } catch (err) {
+    console.error("Erro ao enviar mensagem:", err);
+  }
+});
+
+client.login(TOKEN);
